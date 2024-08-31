48c22fa6ea3b88024ef5ebe8b097b95897e56771
Raz Besaleli <rbesaleli@gmail.com>
2024-08-31 19:36:49 -0400

    reset tvali

--- tvali/__init__.py
+++ tvali/__init__.py
@@ -1,18 +1,1 @@
-from .records.records import (
-    SystemRecord,
-    SubsystemRecord,
-    ComponentRecord,
-    SubcomponentRecord,
-)
-
-from .records.types import (
-    Record,
-    IO,
-    Parameters,
-    Input,
-    Output,
-    Feedback,
-    Metadata
-)
-
-from .config import init, TvaliConfig
+"""Tvali."""
