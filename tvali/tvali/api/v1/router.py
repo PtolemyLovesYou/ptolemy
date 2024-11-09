d4e4923352d38b3c76273e971c42ce8f1afa0be6
Raz Besaleli <rbesaleli@gmail.com>
2024-11-08 23:19:06 -0500

    add v1 router

---
+++ tvali/tvali/api/v1/router.py
@@ -0,0 +1,7 @@
+"""API v1 router."""
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/v1",
+    tags=["v1"],
+)
--- tvali/tvali/main.py
+++ tvali/tvali/main.py
@@ -1,6 +1,7 @@
 """Tvali API"""
 from fastapi import FastAPI
 from .api.health import router as health_router
+from .api.v1.router import router as v1_router
 from .db.session import Base, engine
 from .db.models import *
 
@@ -8,3 +9,4 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(health_router)
+app.include_router(v1_router)
