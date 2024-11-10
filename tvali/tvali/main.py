ef7d999128f795e3f79e793d8afa3a548c19942c
Raz Besaleli <rbesaleli@gmail.com>
2024-11-10 11:43:07 -0500

    docs

--- tvali/tvali/main.py
+++ tvali/tvali/main.py
@@ -4,10 +4,18 @@ from fastapi import FastAPI
 from .api.health import router as health_router
 from .api.v1.router import router as v1_router
 from .db.session import Base, engine
-from .db.models import *
+
+# import all models so they actually get created
+from .db.models import * # pylint: disable=unused-wildcard-import,wildcard-import
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Tvali API",
+    description="Tvali API",
+    version="0.0.1",
+    docs_url="/swagger",
+)
+
 app.include_router(health_router)
 app.include_router(v1_router)
