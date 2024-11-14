df90bc86672050320ad63735cfec1de34c2fdc6f
Raz Besaleli <rbesaleli@gmail.com>
2024-11-14 12:56:42 -0500

    unit tests

--- tvali/tests/api/test_create.py
+++ tvali/tests/api/test_create.py
@@ -61,11 +61,44 @@ def test_create(tier: Tier, log_type: LogType):
     assert response.status_code == 200
     assert response.json()[0]["id"] == event_data["id"]
 
+@pytest.mark.parametrize("log_type", [*LogType])
 @pytest.mark.parametrize("tier", [*Tier])
 @pytest.mark.dependency(name="test_get_log", depends=["test_create"])
-def test_get_log(tier: Tier):
+def test_get_log(tier: Tier, log_type: LogType):
     """Test get log."""
-    response = client.get(f"/v1/log/{tier.value}/event/?id={tier_ids[tier]}")
+    if log_type == LogType.EVENT:
+        query = f"?id={tier_ids[tier]}"
+    else:
+        query = f"?{tier}_event_id={tier_ids[tier]}"
+
+    response = client.get(f"/v1/log/{tier.value}/{log_type}/{query}")
 
     assert response.status_code == 200, response.text
-    assert response.json()[0]["id"] == tier_ids[tier]
+
+    if log_type == LogType.EVENT:
+        assert response.json()[0]["id"] == tier_ids[tier]
+    else:
+        assert response.json()[0][f"{tier}_event_id"] == tier_ids[tier]
+
+@pytest.mark.parametrize("tier", [*reversed(Tier)])
+@pytest.mark.dependency(name="test_delete_log", depends=["test_get_log"])
+def test_delete_log(tier: Tier):
+    """Test delete log."""
+    response = client.delete(f"/v1/log/{tier.value}/event/{tier_ids[tier]}")
+
+    assert response.status_code == 200
+
+    for log_type in LogType:
+        query = f"?{'id' if log_type == LogType.EVENT else f'{tier}_event_id'}={tier_ids[tier]}"
+
+        response = client.get(f"/v1/log/{tier.value}/{log_type}/{query}")
+
+        assert response.status_code == 200
+        assert len(response.json()) == 0
+
+@pytest.mark.parametrize("tier", [*Tier])
+def test_delete_not_found(tier: Tier):
+    """Test delete log after it has been deleted."""
+    response = client.delete(f"/v1/log/{tier.value}/event/{tier_ids[tier]}")
+
+    assert response.status_code == 404
