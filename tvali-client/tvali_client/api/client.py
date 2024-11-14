2ba96cd909069044bd5b8c9281e44f2ebdfc53ad
Raz Besaleli <rbesaleli@gmail.com>
2024-11-13 23:41:20 -0500

    update

--- tvali-client/tvali_client/api/client.py
+++ tvali-client/tvali_client/api/client.py
@@ -1,31 +1,11 @@
 """REST API client."""
 
-from typing import ClassVar
-from pydantic import create_model
-from ..client import TvaliClient
+from ..client import client_factory
 from .log import TvaliLog
 from .config import TvaliConfig
 
-
-class Tvali(TvaliClient, TvaliConfig):
-    """API client."""
-
-    @property
-    def transport_config(self) -> TvaliConfig:
-        """Get transport config.
-
-        Returns:
-            TvaliConfig: Transport config
-        """
-        return TvaliConfig(
-            **self.model_dump(
-                include=TvaliConfig.model_fields.keys(),
-            )
-        )
-
-    def log_class(self) -> type[TvaliLog]:
-        return create_model(
-            "APILog",
-            __base__=TvaliLog,
-            TRANSPORT_CONFIG=(ClassVar[TvaliConfig], self.transport_config),
-        )
+Tvali = client_factory(
+    "Tvali",
+    TvaliLog,
+    TvaliConfig
+)
