8bba39c1fc101837f1b4d7bac3edec33fa59494a
Raz Besaleli <rbesaleli@gmail.com>
2024-11-14 18:58:40 -0500

    requirements check

--- tvali/tvali/utils/require.py
+++ tvali/tvali/utils/require.py
@@ -1,28 +1,31 @@
 """Function for requiring extras."""
-from typing import List
+from typing import List, Callable, Any
 import functools
 import pkg_resources
 
-def require(*packages: str, required_extras: List[str]):
+def require(
+    *packages: str,
+    required_extras: List[str]
+) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     """
-    Function for requiring extras.
+    Decorator function for requiring extras.
 
-    Usage:
-    >>> @require("requests", required_extras=["http"])
-    ... def fetch_url(url):
-    ...     import requests
-    ...     return requests.get(url)
+    Args:
+        packages (List[str]): List of packages to check for installation.
+        required_extras (List[str]): List of extras to require for installation.
 
-    :param packages: List of packages that need to be installed
-    :param required_extras: List of extras that need to be installed
+    Returns:
+        Callable[[Callable[..., Any]], Callable[..., Any]]: Decorator function to be used.
     """
-    def decorator(func):
+    def decorator(func) -> Callable[..., Any]:
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args, **kwargs) -> Any:
             try:
+                # Check if the required packages are installed
                 pkg_resources.require(*packages)
                 return func(*args, **kwargs)
             except pkg_resources.DistributionNotFound as e:
+                # Raise an error if the required extras are not installed
                 raise ImportError(
                     f"This functionality requires the following extras to be installed: {required_extras}. To install, run `pip install tvali[{','.join(required_extras)}]`"
                     ) from e
