f291340eab0f351548972cc23242a53c093ddfc5
Raz Besaleli <rbesaleli@gmail.com>
2024-11-14 19:33:33 -0500

    remove graphql for now

--- tvali/tvali/api/main.py
+++ tvali/tvali/api/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 from .core.lifespan import lifespan
 from .routes.health import router as health_router
 from .routes.v1.router import router as v1_router
-from .routes.graphql.router import router as graphql_router
 
 # import all models so they actually get created
 from .db.models import *  # pylint: disable=unused-wildcard-import,wildcard-import
@@ -18,5 +17,4 @@ app = FastAPI(
 )
 
 app.include_router(health_router)
-app.include_router(graphql_router)
 app.include_router(v1_router)
