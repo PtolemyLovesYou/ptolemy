eed176f267d44eb21c5d93c64d052cdefb5d1249
Raz Besaleli <rbesaleli@gmail.com>
2024-11-19 17:33:33 -0500

    update names

--- tvali/tvali/__main__.py
+++ tvali/tvali/__main__.py
@@ -10,13 +10,13 @@ logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    "command", type=str, help="Command to execute", choices=["subscriber"]
+    "command", type=str, help="Command to execute", choices=["publisher"]
 )
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    if args.command == "subscriber":
-        from tvali.subscriber.main import listen
+    if args.command == "publisher":
+        from tvali.publisher.main import listen
 
         asyncio.run(listen(Redis(host="redis", port=6379, db=0), "tvali"))
