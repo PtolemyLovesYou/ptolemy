75c46a5c377c5080ad2a20bccb6ce07806edd5ba
Raz Besaleli <rbesaleli@gmail.com>
2024-11-25 18:40:08 -0500

    context manager for grpc channel

--- tvali/tvali/client/client/grpc.py
+++ tvali/tvali/client/client/grpc.py
@@ -1,27 +1,31 @@
 """Observer client."""
 
-from typing import Optional, List
+from typing import AsyncGenerator, List
 import os
+from contextlib import asynccontextmanager
 from grpc import aio as grpc
 from ...utils import Record
 from ...proto import observer_pb2 as observer, observer_pb2_grpc as observer_grpc
 
 
-def get_grpc_channel() -> grpc.Channel:
+@asynccontextmanager
+async def get_grpc_channel() -> AsyncGenerator[grpc.Channel, None]:
     """Get gRPC channel."""
-    return grpc.insecure_channel(
+    async with grpc.insecure_channel(
         f"{os.getenv("OBSERVER_HOST")}:{os.getenv('OBSERVER_PORT')}"
-    )
+    ) as f:
+        yield f
 
 
 async def push_records(
-    records: List[Record], channel: Optional[grpc.Channel] = None
+    records: List[Record]
 ) -> None:
     """Push records."""
-    stub = observer_grpc.ObserverStub(channel or get_grpc_channel())
+    async with get_grpc_channel() as channel:
+        stub = observer_grpc.ObserverStub(channel)
 
-    await stub.Publish(
-        observer.PublishRequest(  # pylint: disable=no-member
-            records=[r.proto() for r in records]
+        await stub.Publish(
+            observer.PublishRequest(  # pylint: disable=no-member
+                records=[r.proto() for r in records]
+            )
         )
-    )
