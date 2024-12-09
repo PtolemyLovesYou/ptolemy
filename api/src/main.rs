020a51961b88570871b8c86bd3c5cc923e8d7ef9
Raz Besaleli <rbesaleli@gmail.com>
2024-12-09 08:05:21 +0400

    logging

--- api/src/main.rs
+++ api/src/main.rs
@@ -10,8 +10,8 @@ pub struct ApiConfig {
 
 impl ApiConfig {
     fn new() -> ApiConfig {
-        let host = std::env::var("API_HOST").expect("API_HOST must be set.");
-        let port = std::env::var("API_PORT").expect("API_PORT must be set.");
+        let host = std::env::var("PTOLEMY_API_HOST").expect("API_HOST must be set.");
+        let port = std::env::var("PTOLEMY_API_PORT").expect("API_PORT must be set.");
 
         ApiConfig {
             host: host,
@@ -22,15 +22,15 @@ impl ApiConfig {
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
     // build application
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     let api_config = ApiConfig::new();
 
     // run with hyper
     let server_url = format!("{}:{}", api_config.host, api_config.port);
-    let listener = tokio::net::TcpListener::bind(server_url).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(&server_url).await.unwrap();
+    log::info!("Serving at {}", &server_url);
     axum::serve(listener, app).await.unwrap();
-
-    println!("Serving at http://{}:{}", "0.0.0.0", "3000");
-
 }
