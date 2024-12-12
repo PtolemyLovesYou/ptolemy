e1f9bb6706c4286503977566127d050b13350328
Raz Besaleli <80226248+besaleli@users.noreply.github.com>
2024-12-12 10:54:57 -0500

    Switch to clickhouse variant types (#17)

--- goose/clickhouse/20241207133752_init_records_table.sql
+++ goose/clickhouse/20241207133752_init_records_table.sql
@@ -5,31 +5,75 @@
 
 create database if not exists ${PTOLEMY_CLICKHOUSE_DATABASE};
 
-
-
-create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.records (
+create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__event (
     tier Enum('UNDECLARED_TIER' = 0, 'SYSTEM' = 1, 'SUBSYSTEM' = 2, 'COMPONENT' = 3, 'SUBCOMPONENT' = 4) NOT NULL,
-    log_type Enum('UNDECLARED_LOG_TYPE' = 0, 'EVENT' = 1, 'RUNTIME' = 2, 'INPUT' = 3, 'OUTPUT' = 4, 'FEEDBACK' = 5, 'METADATA' = 6) NOT NULL,
     parent_id String NOT NULL,
     id String NOT NULL,
     name String,
-    parameters String,
+    parameters JSON,
     version String,
     environment String,
+    created_at DateTime64(9) default now64()
+) ENGINE = MergeTree() ORDER BY (tier, created_at);
+
+create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__runtime (
+    tier Enum('UNDECLARED_TIER' = 0, 'SYSTEM' = 1, 'SUBSYSTEM' = 2, 'COMPONENT' = 3, 'SUBCOMPONENT' = 4) NOT NULL,
+    parent_id String NOT NULL,
+    id String NOT NULL,
     start_time String,
     end_time String,
     error_type String,
-    error_content String,
+    error_value String,
+    created_at DateTime64(9) default now64()
+) ENGINE = MergeTree() ORDER BY (tier, created_at);
+
+create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__input (
+    tier Enum('UNDECLARED_TIER' = 0, 'SYSTEM' = 1, 'SUBSYSTEM' = 2, 'COMPONENT' = 3, 'SUBCOMPONENT' = 4) NOT NULL,
+    parent_id String NOT NULL,
+    id String NOT NULL,
+    field_name String,
+    field_value Variant(String, Decimal64(18), Bool, UUID, JSON),
+    created_at DateTime64(9) default now64()
+) ENGINE = MergeTree() ORDER BY (tier, created_at);
+
+create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__output (
+    tier Enum('UNDECLARED_TIER' = 0, 'SYSTEM' = 1, 'SUBSYSTEM' = 2, 'COMPONENT' = 3, 'SUBCOMPONENT' = 4) NOT NULL,
+    parent_id String NOT NULL,
+    id String NOT NULL,
+    field_name String,
+    field_value Variant(String, Decimal64(18), Bool, UUID, JSON),
+    created_at DateTime64(9) default now64()
+) ENGINE = MergeTree() ORDER BY (tier, created_at);
+
+create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__feedback (
+    tier Enum('UNDECLARED_TIER' = 0, 'SYSTEM' = 1, 'SUBSYSTEM' = 2, 'COMPONENT' = 3, 'SUBCOMPONENT' = 4) NOT NULL,
+    parent_id String NOT NULL,
+    id String NOT NULL,
+    field_name String,
+    field_value Variant(String, Decimal64(18), Bool, UUID, JSON),
+    created_at DateTime64(9) default now64()
+) ENGINE = MergeTree() ORDER BY (tier, created_at);
+
+create or replace table ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__metadata (
+    tier Enum('UNDECLARED_TIER' = 0, 'SYSTEM' = 1, 'SUBSYSTEM' = 2, 'COMPONENT' = 3, 'SUBCOMPONENT' = 4) NOT NULL,
+    parent_id String NOT NULL,
+    id String NOT NULL,
     field_name String,
     field_value String,
     created_at DateTime64(9) default now64()
-) ENGINE = MergeTree() ORDER BY (tier, log_type, created_at);
+) ENGINE = MergeTree() ORDER BY (tier, created_at);
 
 -- #####################################################################
 
 -- +goose Down
 
-drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.records;
+drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__event;
+drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__runtime;
+drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__input;
+drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__output;
+drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__feedback;
+drop table if exists ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__metadata;
+
 drop database if exists ${PTOLEMY_CLICKHOUSE_DATABASE};
 
 -- +goose envsub off
--- goose/configure.sh
+++ goose/configure.sh
@@ -1,7 +1,7 @@
 if [[ $DB = "clickhouse" ]]; then
     echo "Setting driver to clickhouse" && \
         export GOOSE_DRIVER="clickhouse" && \
-        export GOOSE_DBSTRING="http://clickhouse:8123/?enable_json_type=1&enable_dynamic_type=1" && \
+        export GOOSE_DBSTRING="http://clickhouse:8123/?enable_json_type=1&enable_variant_type=1" && \
         export GOOSE_MIGRATION_DIR="clickhouse";
 else
     echo "Invalid DB: $DB" && exit 1;
