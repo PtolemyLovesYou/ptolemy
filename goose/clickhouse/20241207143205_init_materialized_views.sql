831aea1464a15eda1a5345029038c2ba5541ad04
Raz Besaleli <rbesaleli@gmail.com>
2024-12-12 19:50:33 +0400

    update sql schema

--- goose/clickhouse/20241207143205_init_materialized_views.sql
+++ goose/clickhouse/20241207143205_init_materialized_views.sql
@@ -9,56 +9,48 @@ select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     name,
-    CAST(COALESCE(parameters, '{}') as JSON) as parameters,
+    parameters,
     version,
     environment,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 1 and log_type = 1
-SETTINGS
-    enable_json_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__event
+where tier = 1;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_event_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_event as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     name,
-    CAST(COALESCE(parameters, '{}') as JSON) as parameters,
+    parameters,
     version,
     environment,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 2 and log_type = 1
-SETTINGS
-    enable_json_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__event
+where tier = 2;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.component_event_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.component_event as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     name,
-    CAST(COALESCE(parameters, '{}') as JSON) as parameters,
+    parameters,
     version,
     environment,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 3 and log_type = 1
-SETTINGS
-    enable_json_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__event
+where tier = 3;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_event_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_event as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     name,
-    CAST(COALESCE(parameters, '{}') as JSON) as parameters,
+    parameters,
     version,
     environment,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 4 and log_type = 1
-SETTINGS
-    enable_json_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__event
+where tier = 4;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.system_runtime_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.system_runtime as
 select
@@ -67,10 +59,10 @@ select
     toDateTime64(start_time, 9) as start_time,
     toDateTime64(end_time, 9) as end_time,
     error_type,
-    error_content,
+    error_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 1 and log_type = 2;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__runtime
+where tier = 1;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_runtime_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_runtime as
 select
@@ -79,10 +71,10 @@ select
     toDateTime64(start_time, 9) as start_time,
     toDateTime64(end_time, 9) as end_time,
     error_type,
-    error_content,
+    error_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 2 and log_type = 2;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__runtime
+where tier = 2;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.component_runtime_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.component_runtime as
 select
@@ -91,10 +83,10 @@ select
     toDateTime64(start_time, 9) as start_time,
     toDateTime64(end_time, 9) as end_time,
     error_type,
-    error_content,
+    error_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 3 and log_type = 2;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__runtime
+where tier = 3;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_runtime_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_runtime as
 select
@@ -103,154 +95,132 @@ select
     toDateTime64(start_time, 9) as start_time,
     toDateTime64(end_time, 9) as end_time,
     error_type,
-    error_content,
+    error_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 4 and log_type = 2;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__runtime
+where tier = 4;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.system_input_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.system_input as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 1 and log_type = 3
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__input
+where tier = 1
 SETTINGS
-    enable_dynamic_type = 1;
+    enable_variant_type = 1;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_input_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_input as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 2 and log_type = 3
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__input
+where tier = 2;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.component_input_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.component_input as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 3 and log_type = 3
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__input
+where tier = 3;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_input_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_input as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 4 and log_type = 3
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__input
+where tier = 4;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.system_output_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.system_output as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 1 and log_type = 4
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__output
+where tier = 1;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_output_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_output as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 2 and log_type = 4
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__output
+where tier = 2;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.component_output_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.component_output as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 3 and log_type = 4
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__output
+where tier = 3;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_output_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_output as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 4 and log_type = 4
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__output
+where tier = 4;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.system_feedback_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.system_feedback as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 1 and log_type = 5
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__feedback
+where tier = 1;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_feedback_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_feedback as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 2 and log_type = 5
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__feedback
+where tier = 2;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.component_feedback_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.component_feedback as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 3 and log_type = 5
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__feedback
+where tier = 3;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_feedback_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_feedback as
 select
     toUUID(parent_id) as parent_id,
     toUUID(id) as id,
     field_name,
-    field_value::Dynamic as field_value,
+    field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 4 and log_type = 5
-SETTINGS
-    enable_dynamic_type = 1;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__feedback
+where tier = 4;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.system_metadata_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.system_metadata as
 select
@@ -259,8 +229,8 @@ select
     field_name,
     field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 1 and log_type = 6;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__metadata
+where tier = 1;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_metadata_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subsystem_metadata as
 select
@@ -269,8 +239,8 @@ select
     field_name,
     field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 2 and log_type = 6;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__metadata
+where tier = 2;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.component_metadata_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.component_metadata as
 select
@@ -279,8 +249,8 @@ select
     field_name,
     field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 3 and log_type = 6;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__metadata
+where tier = 3;
 
 create materialized view ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_metadata_mv to ${PTOLEMY_CLICKHOUSE_DATABASE}.subcomponent_metadata as
 select
@@ -289,8 +259,8 @@ select
     field_name,
     field_value,
     created_at
-from ${PTOLEMY_CLICKHOUSE_DATABASE}.records
-where tier = 4 and log_type = 6;
+from ${PTOLEMY_CLICKHOUSE_DATABASE}.stg__metadata
+where tier = 4;
 
 -- #####################################################################
 
