efa4f6c5a15ec677facd0189ec549ccc3b248aed
Raz Besaleli <rbesaleli@gmail.com>
2024-12-17 13:40:40 +0400

    refactor

--- api/configure.sh
+++ api/configure.sh
@@ -1,12 +1,5 @@
-# Configure Goose env vars
-if [[ $DB = "clickhouse" ]]; then
-    echo "Setting driver to clickhouse" && \
-        export GOOSE_DRIVER="clickhouse" && \
-        export GOOSE_DBSTRING="http://clickhouse:8123/?enable_json_type=1&enable_variant_type=1" && \
-        export GOOSE_MIGRATION_DIR="migrations/clickhouse";
-else
-    echo "Invalid DB: $DB" && exit 1;
-fi
-
 # Configure Diesel env vars
 export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB
+
+# Configure Goose env vars for Clickhouse
+export GOOSE_DBSTRING="http://clickhouse:8123/?enable_json_type=1&enable_variant_type=1"
