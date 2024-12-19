02353f43e200ad9a418b2a68bdf56b256b9b391e
Raz Besaleli <rbesaleli@gmail.com>
2024-12-19 11:50:38 +0400

    connect to postgres

--- api/src/observer/service.rs
+++ api/src/observer/service.rs
@@ -8,6 +8,7 @@ use ptolemy_core::generated::observer::{
     observer_server::Observer, PublishRequest, PublishResponse, Record
 };
 use tonic::{Request, Response, Status};
+use diesel_async::RunQueryDsl;
 use crate::state::AppState;
 use crate::models::events::EventRow;
 
@@ -22,11 +23,29 @@ impl MyObserver {
     }
 }
 
+macro_rules! insert_records {
+    ($conn:ident, $vals:ident, $table:ident) => {
+        if !$vals.is_empty() {
+            match diesel::insert_into(crate::models::schema::$table::table)
+                .values(&$vals)
+                .execute(&mut $conn)
+                .await {
+                    Ok(_) => {
+                        debug!("Pushed {} records to Postgres", $vals.len());
+                    },
+                    Err(e) => {
+                        error!("Failed to push records to Postgres: {}", e);
+                    }
+                };
+        };
+    }
+}
+
 // #[instrument]
 async fn insert_rows(state: Arc<AppState>, records: Vec<Record>) {
-    let _conn = state.pg_pool.get().await.unwrap();
+    let mut conn = state.pg_pool.get().await.unwrap();
 
-    let _parsed_records: Vec<EventRow> = records
+    let parsed_records: Vec<EventRow> = records
         .into_iter()
         .filter_map(|r| {
             match EventRow::from_record(&r) {
@@ -41,6 +60,63 @@ async fn insert_rows(state: Arc<AppState>, records: Vec<Record>) {
             }
         }
     ).collect();
+
+    let mut system_event_rows = Vec::new();
+    let mut system_runtime_rows = Vec::new();
+    let mut system_io_rows = Vec::new();
+    let mut system_metadata_rows = Vec::new();
+
+    let mut subsystem_event_rows = Vec::new();
+    let mut subsystem_runtime_rows = Vec::new();
+    let mut subsystem_io_rows = Vec::new();
+    let mut subsystem_metadata_rows = Vec::new();
+    
+    let mut component_event_rows = Vec::new();
+    let mut component_runtime_rows = Vec::new();
+    let mut component_io_rows = Vec::new();
+    let mut component_metadata_rows = Vec::new();
+
+    let mut subcomponent_event_rows = Vec::new();
+    let mut subcomponent_runtime_rows = Vec::new();
+    let mut subcomponent_io_rows = Vec::new();
+    let mut subcomponent_metadata_rows = Vec::new();
+
+    for r in parsed_records {
+        match r {
+            EventRow::SystemEvent(e) => system_event_rows.push(e),
+            EventRow::SystemRuntime(e) => system_runtime_rows.push(e),
+            EventRow::SystemIO(e) => system_io_rows.push(e),
+            EventRow::SystemMetadata(e) => system_metadata_rows.push(e),
+            EventRow::SubsystemEvent(e) => subsystem_event_rows.push(e),
+            EventRow::SubsystemRuntime(e) => subsystem_runtime_rows.push(e),
+            EventRow::SubsystemIO(e) => subsystem_io_rows.push(e),
+            EventRow::SubsystemMetadata(e) => subsystem_metadata_rows.push(e),
+            EventRow::ComponentEvent(e) => component_event_rows.push(e),
+            EventRow::ComponentRuntime(e) => component_runtime_rows.push(e),
+            EventRow::ComponentIO(e) => component_io_rows.push(e),
+            EventRow::ComponentMetadata(e) => component_metadata_rows.push(e),
+            EventRow::SubcomponentEvent(e) => subcomponent_event_rows.push(e),
+            EventRow::SubcomponentRuntime(e) => subcomponent_runtime_rows.push(e),
+            EventRow::SubcomponentIO(e) => subcomponent_io_rows.push(e),
+            EventRow::SubcomponentMetadata(e) => subcomponent_metadata_rows.push(e),
+        }
+    };
+
+    insert_records!(conn, system_event_rows, system_event);
+    insert_records!(conn, system_runtime_rows, system_runtime);
+    insert_records!(conn, system_io_rows, system_io);
+    insert_records!(conn, system_metadata_rows, system_metadata);
+    insert_records!(conn, subsystem_event_rows, subsystem_event);
+    insert_records!(conn, subsystem_runtime_rows, subsystem_runtime);
+    insert_records!(conn, subsystem_io_rows, subsystem_io);
+    insert_records!(conn, subsystem_metadata_rows, subsystem_metadata);
+    insert_records!(conn, component_event_rows, component_event);
+    insert_records!(conn, component_runtime_rows, component_runtime);
+    insert_records!(conn, component_io_rows, component_io);
+    insert_records!(conn, component_metadata_rows, component_metadata);
+    insert_records!(conn, subcomponent_event_rows, subcomponent_event);
+    insert_records!(conn, subcomponent_io_rows, subcomponent_io);
+    insert_records!(conn, subcomponent_metadata_rows, subcomponent_metadata);
 }
 
 #[tonic::async_trait]
