24ad984978a4fffc8b8040dfa7dbcc01bf7015eb
Raz Besaleli <rbesaleli@gmail.com>
2024-12-23 13:41:56 +0400

    iam models

--- api/src/models/enums.rs
+++ api/src/models/enums.rs
@@ -1,4 +1,4 @@
-use crate::generated::schema::sql_types::FieldValueType;
+use crate::generated::schema::sql_types::{FieldValueType, WorkspaceRole, ApiKeyPermission};
 use diesel::deserialize::FromSql;
 use diesel::serialize::{IsNull, Output, ToSql};
 use diesel::{
@@ -74,6 +74,69 @@ impl FromSql<FieldValueType, Pg> for FieldValueTypeEnum {
             b"float" => Ok(FieldValueTypeEnum::Float),
             b"bool" => Ok(FieldValueTypeEnum::Bool),
             b"json" => Ok(FieldValueTypeEnum::Json),
+            _ => Err("Unrecognized enum variant".into()),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, FromSqlRow, AsExpression, Eq, Serialize, Deserialize)]
+#[diesel(sql_type = WorkspaceRole)]
+pub enum WorkspaceRoleEnum {
+    Reader,
+    Writer,
+    Manager,
+    Admin,
+}
+
+impl ToSql<FieldValueType, Pg> for WorkspaceRoleEnum {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> diesel::serialize::Result {
+        match *self {
+            WorkspaceRoleEnum::Reader => out.write_all(b"reader")?,
+            WorkspaceRoleEnum::Writer => out.write_all(b"writer")?,
+            WorkspaceRoleEnum::Manager => out.write_all(b"manager")?,
+            WorkspaceRoleEnum::Admin => out.write_all(b"admin")?,
+        }
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<FieldValueType, Pg> for WorkspaceRoleEnum {
+    fn from_sql(bytes: PgValue<'_>) -> diesel::deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"reader" => Ok(WorkspaceRoleEnum::Reader),
+            b"writer" => Ok(WorkspaceRoleEnum::Writer),
+            b"manager" => Ok(WorkspaceRoleEnum::Manager),
+            b"admin" => Ok(WorkspaceRoleEnum::Admin),
+            _ => Err("Unrecognized enum variant".into()),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, FromSqlRow, AsExpression, Eq, Serialize, Deserialize)]
+#[diesel(sql_type = ApiKeyPermission)]
+pub enum ApiKeyPermissionEnum {
+    ReadOnly,
+    WriteOnly,
+    ReadWrite,
+}
+
+impl ToSql<ApiKeyPermission, Pg> for ApiKeyPermissionEnum {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> diesel::serialize::Result {
+        match *self {
+            ApiKeyPermissionEnum::ReadOnly => out.write_all(b"read_only")?,
+            ApiKeyPermissionEnum::WriteOnly => out.write_all(b"write_only")?,
+            ApiKeyPermissionEnum::ReadWrite => out.write_all(b"read_write")?,
+        }
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<ApiKeyPermission, Pg> for ApiKeyPermissionEnum {
+    fn from_sql(bytes: PgValue<'_>) -> diesel::deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"read_only" => Ok(ApiKeyPermissionEnum::ReadOnly),
+            b"write_only" => Ok(ApiKeyPermissionEnum::WriteOnly),
+            b"read_write" => Ok(ApiKeyPermissionEnum::ReadWrite),
             _ => Err("Unrecognized enum variant".into()),
         }
     }
--- api/src/models/iam.rs
+++ api/src/models/iam.rs
@@ -2,6 +2,7 @@ use chrono::{naive::serde::ts_microseconds, NaiveDateTime};
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use crate::models::enums::{ApiKeyPermissionEnum, WorkspaceRoleEnum};
 
 #[derive(Debug, Queryable, Selectable, Serialize, Deserialize)]
 #[diesel(table_name = crate::generated::schema::workspace)]
@@ -23,4 +24,55 @@ pub struct Workspace {
 pub struct WorkspaceCreate {
     name: String,
     description: Option<String>,
+}
+
+#[derive(Debug, Queryable, Selectable, Serialize, Deserialize)]
+#[diesel(table_name = crate::generated::schema::users)]
+pub struct User {
+    pub id: Uuid,
+    pub username: String,
+    pub password_hash: String,
+    pub display_name: Option<String>,
+    pub is_sysadmin: bool,
+    pub is_admin: bool,
+}
+
+#[derive(Debug, Queryable, Selectable, Serialize, Deserialize)]
+#[diesel(table_name = crate::generated::schema::users)]
+pub struct UserCreate {
+    pub username: String,
+    pub password_hash: String,
+    pub display_name: Option<String>,
+    pub is_sysadmin: bool,
+    pub is_admin: bool,
+}
+
+#[derive(Debug, Queryable, Selectable, Insertable, Serialize, Deserialize)]
+#[diesel(table_name = crate::generated::schema::workspace_user)]
+pub struct WorkspaceUser {
+    pub user_id: Uuid,
+    pub workspace_id: Uuid,
+    pub role: WorkspaceRoleEnum,
+}
+
+#[derive(Debug, Queryable, Selectable, Insertable, Serialize, Deserialize)]
+#[diesel(table_name = crate::generated::schema::user_api_key)]
+pub struct UserApiKey {
+    #[diesel(treat_none_as_default_value = true)]
+    pub id: Option<Uuid>,
+    pub user_id: Uuid,
+    pub key_hash: String,
+    pub permissions: ApiKeyPermissionEnum,
+    pub expires_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Queryable, Selectable, Insertable, Serialize, Deserialize)]
+#[diesel(table_name = crate::generated::schema::service_api_key)]
+pub struct ServiceApiKey {
+    #[diesel(treat_none_as_default_value = true)]
+    pub id: Option<Uuid>,
+    pub workspace_id: Uuid,
+    pub key_hash: String,
+    pub permissions: ApiKeyPermissionEnum,
+    pub expires_at: Option<NaiveDateTime>,
 }
