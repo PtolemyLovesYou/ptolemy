a15a6013556b8bafa16570520061f8877d62b91d
Raz Besaleli <rbesaleli@gmail.com>
2024-12-29 23:24:49 +0400

    pydantic model

--- prototype/prototype/profile.py
+++ prototype/prototype/profile.py
@@ -1,7 +1,14 @@
 """Profile management view."""
 import streamlit as st
+from .models import User
 
 @st.fragment
 def profile_view():
     """Profile view."""
-    st.write("Profile view")
+    current_user = User.current_user()
+
+    st.subheader("Profile")
+    st.text_input("Username", value=current_user.username, disabled=True)
+    st.text_input("Display Name", value=current_user.display_name, disabled=True)
+
+    st.subheader("API Keys")
