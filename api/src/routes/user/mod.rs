f1b96aa88b54782bf121254a2a639162b03d183c
Raz Besaleli <rbesaleli@gmail.com>
2024-12-29 23:07:57 +0400

    docs

--- api/src/routes/user/mod.rs
+++ api/src/routes/user/mod.rs
@@ -6,6 +6,13 @@ use crate::state::AppState;
 pub mod user;
 pub mod user_api_key;
 
+/// Generates a router for user-related routes.
+///
+/// The routes included are:
+/// - All user routes (see user::user_base_router)
+/// - All user API key routes (see user_api_key::user_api_key_router)
+///
+/// Note that the user API key routes are nested under `/:user_id/api_key/`.
 pub async fn user_router(state: &Arc<AppState>) -> Router {
     Router::new()
         .nest("/", self::user::user_base_router(state).await)
