4aeb9f4943e32d76e32ad3b1746a228f4b8d0b49
Raz Besaleli <rbesaleli@gmail.com>
2024-12-30 12:58:46 +0400

    update migrations

--- api/diesel/2024-12-17-195437_event_tables/down.sql
+++ api/diesel/2024-12-17-195437_event_tables/down.sql
@@ -1,18 +1,10 @@
 -- This file should undo anything in `up.sql`
-drop table subcomponent_runtime;
-drop table subcomponent_io;
-drop table subcomponent_metadata;
+drop table runtime;
+drop table io;
+drop table metadata;
 drop table subcomponent_event;
-drop table component_runtime;
-drop table component_io;
-drop table component_metadata;
 drop table component_event;
-drop table subsystem_runtime;
-drop table subsystem_io;
-drop table subsystem_metadata;
 drop table subsystem_event;
-drop table system_runtime;
-drop table system_io;
-drop table system_metadata;
 drop table system_event;
 drop type field_value_type;
+drop type tier;
--- api/src/generated/records_schema.rs
+++ api/src/generated/records_schema.rs
@@ -2,10 +2,6 @@
 
 pub mod sql_types {
     #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "field_value_type"))]
-    pub struct FieldValueType;
-
-    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "workspace_role"))]
     pub struct WorkspaceRole;
 }
@@ -13,200 +9,52 @@ pub mod sql_types {
 diesel::table! {
     component_event (id) {
         id -> Uuid,
-        parent_id -> Uuid,
+        subsystem_event_id -> Uuid,
         name -> Varchar,
         parameters -> Nullable<Json>,
         #[max_length = 16]
         version -> Nullable<Varchar>,
         #[max_length = 8]
         environment -> Nullable<Varchar>,
-    }
-}
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::FieldValueType;
-
-    component_io (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Nullable<Varchar>,
-        field_value_str -> Nullable<Varchar>,
-        field_value_int -> Nullable<Int8>,
-        field_value_float -> Nullable<Float8>,
-        field_value_bool -> Nullable<Bool>,
-        field_value_json -> Nullable<Json>,
-        field_value_type -> FieldValueType,
-    }
-}
-
-diesel::table! {
-    component_metadata (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Varchar,
-        field_value -> Varchar,
-    }
-}
-
-diesel::table! {
-    component_runtime (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        start_time -> Timestamp,
-        end_time -> Timestamp,
-        error_type -> Nullable<Varchar>,
-        error_content -> Nullable<Varchar>,
     }
 }
 
 diesel::table! {
     subcomponent_event (id) {
         id -> Uuid,
-        parent_id -> Uuid,
+        component_event_id -> Uuid,
         name -> Varchar,
         parameters -> Nullable<Json>,
         #[max_length = 16]
         version -> Nullable<Varchar>,
         #[max_length = 8]
         environment -> Nullable<Varchar>,
-    }
-}
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::FieldValueType;
-
-    subcomponent_io (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Nullable<Varchar>,
-        field_value_str -> Nullable<Varchar>,
-        field_value_int -> Nullable<Int8>,
-        field_value_float -> Nullable<Float8>,
-        field_value_bool -> Nullable<Bool>,
-        field_value_json -> Nullable<Json>,
-        field_value_type -> FieldValueType,
-    }
-}
-
-diesel::table! {
-    subcomponent_metadata (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Varchar,
-        field_value -> Varchar,
-    }
-}
-
-diesel::table! {
-    subcomponent_runtime (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        start_time -> Timestamp,
-        end_time -> Timestamp,
-        error_type -> Nullable<Varchar>,
-        error_content -> Nullable<Varchar>,
     }
 }
 
 diesel::table! {
     subsystem_event (id) {
         id -> Uuid,
-        parent_id -> Uuid,
+        system_event_id -> Uuid,
         name -> Varchar,
         parameters -> Nullable<Json>,
         #[max_length = 16]
         version -> Nullable<Varchar>,
         #[max_length = 8]
         environment -> Nullable<Varchar>,
-    }
-}
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::FieldValueType;
-
-    subsystem_io (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Nullable<Varchar>,
-        field_value_str -> Nullable<Varchar>,
-        field_value_int -> Nullable<Int8>,
-        field_value_float -> Nullable<Float8>,
-        field_value_bool -> Nullable<Bool>,
-        field_value_json -> Nullable<Json>,
-        field_value_type -> FieldValueType,
-    }
-}
-
-diesel::table! {
-    subsystem_metadata (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Varchar,
-        field_value -> Varchar,
-    }
-}
-
-diesel::table! {
-    subsystem_runtime (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        start_time -> Timestamp,
-        end_time -> Timestamp,
-        error_type -> Nullable<Varchar>,
-        error_content -> Nullable<Varchar>,
     }
 }
 
 diesel::table! {
     system_event (id) {
         id -> Uuid,
-        parent_id -> Uuid,
+        workspace_id -> Uuid,
         name -> Varchar,
         parameters -> Nullable<Json>,
         #[max_length = 16]
         version -> Nullable<Varchar>,
         #[max_length = 8]
         environment -> Nullable<Varchar>,
-    }
-}
-
-diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::FieldValueType;
-
-    system_io (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Nullable<Varchar>,
-        field_value_str -> Nullable<Varchar>,
-        field_value_int -> Nullable<Int8>,
-        field_value_float -> Nullable<Float8>,
-        field_value_bool -> Nullable<Bool>,
-        field_value_json -> Nullable<Json>,
-        field_value_type -> FieldValueType,
-    }
-}
-
-diesel::table! {
-    system_metadata (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        field_name -> Varchar,
-        field_value -> Varchar,
-    }
-}
-
-diesel::table! {
-    system_runtime (id) {
-        id -> Uuid,
-        parent_id -> Uuid,
-        start_time -> Timestamp,
-        end_time -> Timestamp,
-        error_type -> Nullable<Varchar>,
-        error_content -> Nullable<Varchar>,
     }
 }
 
@@ -233,41 +81,17 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(component_event -> subsystem_event (parent_id));
-diesel::joinable!(component_io -> component_event (parent_id));
-diesel::joinable!(component_metadata -> component_event (parent_id));
-diesel::joinable!(component_runtime -> component_event (parent_id));
-diesel::joinable!(subcomponent_event -> component_event (parent_id));
-diesel::joinable!(subcomponent_io -> subcomponent_event (parent_id));
-diesel::joinable!(subcomponent_metadata -> subcomponent_event (parent_id));
-diesel::joinable!(subcomponent_runtime -> subcomponent_event (parent_id));
-diesel::joinable!(subsystem_event -> system_event (parent_id));
-diesel::joinable!(subsystem_io -> subsystem_event (parent_id));
-diesel::joinable!(subsystem_metadata -> subsystem_event (parent_id));
-diesel::joinable!(subsystem_runtime -> subsystem_event (parent_id));
-diesel::joinable!(system_event -> workspace (parent_id));
-diesel::joinable!(system_io -> system_event (parent_id));
-diesel::joinable!(system_metadata -> system_event (parent_id));
-diesel::joinable!(system_runtime -> system_event (parent_id));
+diesel::joinable!(component_event -> subsystem_event (subsystem_event_id));
+diesel::joinable!(subcomponent_event -> component_event (component_event_id));
+diesel::joinable!(subsystem_event -> system_event (system_event_id));
+diesel::joinable!(system_event -> workspace (workspace_id));
 diesel::joinable!(workspace_user -> workspace (workspace_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     component_event,
-    component_io,
-    component_metadata,
-    component_runtime,
     subcomponent_event,
-    subcomponent_io,
-    subcomponent_metadata,
-    subcomponent_runtime,
     subsystem_event,
-    subsystem_io,
-    subsystem_metadata,
-    subsystem_runtime,
     system_event,
-    system_io,
-    system_metadata,
-    system_runtime,
     workspace,
     workspace_user,
 );
