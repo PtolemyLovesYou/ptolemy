de583729a0d260f5854de957a62399062ae212e6
Raz Besaleli <rbesaleli@gmail.com>
2025-01-02 20:11:07 +0400

    update

--- ptolemy/src/ptolemy/_core.pyi
+++ ptolemy/src/ptolemy/_core.pyi
@@ -5,48 +5,6 @@ from typing import List, Optional, Any, Dict
 class BlockingObserverClient:
     """Blocking Observer Client."""
     def __init__(self, batch_size: int) -> None: ...
-    def queue_event_record(
-        self,
-        tier: str,
-        parent_id: str,
-        name: str,
-        parameters: Optional[dict] = None,
-        version: Optional[str] = None,
-        environment: Optional[str] = None,
-    ) -> str: ...
-    def queue_runtime_record(
-        self,
-        tier: str,
-        parent_id: str,
-        start_time: float,
-        end_time: float,
-        error_type: Optional[str] = None,
-        error_content: Optional[str] = None,
-    ) -> str: ...
-    def queue_input_records(
-        self,
-        tier: str,
-        parent_id: str,
-        data: Dict[str, Any]
-    ) -> None: ...
-    def queue_output_records(
-        self,
-        tier: str,
-        parent_id: str,
-        data: Dict[str, Any]
-    ) -> None: ...
-    def queue_feedback_records(
-        self,
-        tier: str,
-        parent_id: str,
-        data: Dict[str, Any]
-    ) -> None: ...
-    def queue_metadata_records(
-        self,
-        tier: str,
-        parent_id: str,
-        data: Dict[str, str]
-    ) -> None: ...
     def queue(self, records: List[ProtoRecord]) -> bool: ...
     def queue_event(self, record: ProtoRecord) -> bool: ...
     def queue_size(self) -> int: ...
