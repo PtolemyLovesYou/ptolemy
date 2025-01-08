0401f6a6117926bdf10bbb4b368e7951a20b005a
Raz Besaleli <rbesaleli@gmail.com>
2025-01-08 14:50:57 +0400

    update strenum cls

--- ptolemy/src/utils/strenum.rs
+++ ptolemy/src/utils/strenum.rs
@@ -7,9 +7,9 @@ macro_rules! string_enum {
     ($mod_name:ident, $enum_name:ident, $casing:ident, [$($variant:ident),+ $(,)?]) => {
         pub mod $mod_name {
             use pyo3::prelude::*;
-            use once_cell::sync::Lazy;
             use pyo3::types::PyType;
             use pyo3::exceptions::PyValueError;
+            use pyo3::sync::GILOnceCell;
             use std::collections::HashMap;
             use heck::{ToSnakeCase, ToShoutySnakeCase, ToLowerCamelCase, ToPascalCase};
 
@@ -23,34 +23,27 @@ macro_rules! string_enum {
                 }
             }
 
-            static ENUM_PY_CLS_NAME: Lazy<String> = Lazy::new(|| stringify!($enum_name).to_pascal_case());
+            const ENUM_CLS_NAME: &'static str = stringify!($enum_name);
 
-            static VARIANTS: Lazy<HashMap<String, String>> = Lazy::new(|| {
+            static ENUM_PY_CLS: GILOnceCell<Py<PyType>> = GILOnceCell::new();
+
+            fn get_enum_py_cls(py: Python<'_>) -> PyResult<&Bound<'_, PyType>> {
                 let mut variants = HashMap::new();
                 $(
                     variants.insert(stringify!($variant).to_shouty_snake_case(), format_variant(stringify!($variant)));
                 )+
 
-                variants
-            });
-
-            static ENUM_PY_CLS: Lazy<Py<PyType>> = Lazy::new(|| {
-                let enum_cls: PyResult<Py<PyType>> = Python::with_gil(|py| {
-                    let en = py.import("enum")?
-                        .getattr("StrEnum")?
-                        .call1((stringify!($enum_name).to_pascal_case(), VARIANTS.clone(),))?
-                        .downcast::<PyType>()?
-                        .clone()
-                        .unbind();
-
-                    Ok(en)
+                let py_cls = ENUM_PY_CLS.get_or_init(py, || {
+                    py
+                        .import("enum").unwrap()
+                        .getattr("StrEnum").unwrap()
+                        .call1((ENUM_CLS_NAME, variants,)).unwrap()
+                        .downcast_into::<PyType>().unwrap()
+                        .unbind()
                 });
 
-                match enum_cls {
-                    Ok(ec) => ec,
-                    Err(e) => panic!("Failed to create enum class: {}", e),
+                Ok(py_cls.bind(py))
                 }
-            });
 
             #[derive(Debug, Clone, PartialEq)]
             pub enum $enum_name {
@@ -91,17 +84,17 @@ macro_rules! string_enum {
             impl<'py> IntoPyObject<'py> for $enum_name {
                 type Target = PyAny;
                 type Output = Bound<'py, Self::Target>;
-                type Error = std::convert::Infallible;
+                type Error = PyErr;
             
                 fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
                     let s: String = self.into();
-                    let val = ENUM_PY_CLS.bind(py).call1((s,)).unwrap();
+                    let val = get_enum_py_cls(py)?.call1((s,))?;
                     Ok(val)
                 }
             }
 
             pub fn add_enum_to_module<'a>(py: Python<'a>, m: &Bound<'a, PyModule>) -> PyResult<()> {
-                m.add(ENUM_PY_CLS_NAME.clone(), ENUM_PY_CLS.bind(py))?;
+                m.add(ENUM_CLS_NAME, get_enum_py_cls(py)?)?;
                 Ok(())
             }
         }
