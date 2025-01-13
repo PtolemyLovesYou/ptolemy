c3ca7716712e4a41fd2b23a5465bb9805caf1591
Raz Besaleli <rbesaleli@gmail.com>
2025-01-13 17:33:11 +0400

    remove old routes

--- api/src/routes/mod.rs
+++ api/src/routes/mod.rs
@@ -1,11 +1,9 @@
-pub mod auth;
-pub mod base;
 pub mod graphql;
 
 pub async fn get_router(state: &std::sync::Arc<crate::state::AppState>) -> axum::Router {
     axum::Router::new()
-        .nest("/auth", auth::auth_router(state).await)
+        .route("/", axum::routing::get(|| async { "Ptolemy API is up and running <3" }))
+        .route("/ping", axum::routing::get(|| async { "Pong!" }))
         .nest("/graphql", graphql::graphql_router(state).await)
-        .nest("/", base::base_router().await)
         .layer(crate::middleware::trace_layer_rest())
 }
