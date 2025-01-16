48b0e698660604a082e037a428dffa10369e1124
Raz Besaleli <rbesaleli@gmail.com>
2025-01-16 12:44:31 +0400

    refactor middleware model

---
+++ api/src/models/middleware/mod.rs
@@ -0,0 +1,52 @@
+use uuid::Uuid;
+use crate::error::CRUDError;
+
+#[derive(Debug, Clone)]
+pub struct AccessContext {
+    pub api_access_audit_log_id: Option<Uuid>,
+    pub auth_audit_log_id: Option<Uuid>,
+    pub iam_audit_log_id: Option<Uuid>,
+    pub record_audit_log_id: Option<Uuid>,
+}
+
+impl AccessContext {
+    pub fn api_access_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
+        match &self.api_access_audit_log_id {
+            Some(i) => Ok(i),
+            None => {
+                tracing::error!("Audit metadata field api_access_audit_log_id not found");
+                Err(CRUDError::InternalError)
+            }
+        }
+    }
+
+    pub fn auth_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
+        match &self.auth_audit_log_id {
+            Some(i) => Ok(i),
+            None => {
+                tracing::error!("Audit metadata field auth_audit_log_id not found");
+                Err(CRUDError::InternalError)
+            }
+        }
+    }
+
+    pub fn iam_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
+        match &self.iam_audit_log_id {
+            Some(i) => Ok(i),
+            None => {
+                tracing::error!("Audit metadata field iam_audit_log_id not found");
+                Err(CRUDError::InternalError)
+            }
+        }
+    }
+
+    pub fn record_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
+        match &self.api_access_audit_log_id {
+            Some(i) => Ok(i),
+            None => {
+                tracing::error!("Audit metadata field record_audit_log_id not found");
+                Err(CRUDError::InternalError)
+            }
+        }
+    }
+}
--- api/src/models/mod.rs
+++ api/src/models/mod.rs
@@ -2,56 +2,6 @@ pub mod audit;
 pub mod auth;
 pub mod enum_utils;
 pub mod records;
+pub mod middleware;
 
-use uuid::Uuid;
-use crate::error::CRUDError;
-
-#[derive(Debug, Clone)]
-pub struct AccessContext {
-    pub api_access_audit_log_id: Option<Uuid>,
-    pub auth_audit_log_id: Option<Uuid>,
-    pub iam_audit_log_id: Option<Uuid>,
-    pub record_audit_log_id: Option<Uuid>,
-}
-
-impl AccessContext {
-    pub fn api_access_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
-        match &self.api_access_audit_log_id {
-            Some(i) => Ok(i),
-            None => {
-                tracing::error!("Audit metadata field api_access_audit_log_id not found");
-                Err(CRUDError::InternalError)
-            }
-        }
-    }
-
-    pub fn auth_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
-        match &self.auth_audit_log_id {
-            Some(i) => Ok(i),
-            None => {
-                tracing::error!("Audit metadata field auth_audit_log_id not found");
-                Err(CRUDError::InternalError)
-            }
-        }
-    }
-
-    pub fn iam_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
-        match &self.iam_audit_log_id {
-            Some(i) => Ok(i),
-            None => {
-                tracing::error!("Audit metadata field iam_audit_log_id not found");
-                Err(CRUDError::InternalError)
-            }
-        }
-    }
-
-    pub fn record_audit_log_id(&self) -> Result<&Uuid, CRUDError> {
-        match &self.api_access_audit_log_id {
-            Some(i) => Ok(i),
-            None => {
-                tracing::error!("Audit metadata field record_audit_log_id not found");
-                Err(CRUDError::InternalError)
-            }
-        }
-    }
-}
+pub use self::middleware::AccessContext;
