7b66e1c583d5d7a4420db12857b469216e520353
Raz Besaleli <rbesaleli@gmail.com>
2025-02-08 19:30:25 -0500

    custom role

---
+++ api/src/bin/create_duckdb_role.rs
@@ -0,0 +1,33 @@
+use api::state::AppState;
+use diesel_async::RunQueryDsl;
+
+const QUERY: [&str; 4] = [
+    "create role ptolemy_duckdb;",
+    "grant connect on database ptolemy to ptolemy_duckdb;",
+    "grant usage on schema duckdb to ptolemy_duckdb;",
+    "grant select on all tables in schema duckdb to ptolemy_duckdb;",
+];
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let state = match AppState::new().await {
+        Ok(state) => state,
+        Err(e) => panic!("Failed to create app state: {:?}", e),
+    };
+
+    let mut conn = state.get_conn().await.unwrap();
+
+    for stmt in QUERY.clone().into_iter() {
+        println!("Executing: {}", stmt);
+        if stmt.is_empty() {
+            continue;
+        }
+
+        match diesel::sql_query(stmt).execute(&mut conn).await {
+            Ok(_) => continue,
+            Err(e) => println!("Failed to create ptolemy_duckdb role: {}", e),
+        };
+    }
+
+    Ok(())
+}
