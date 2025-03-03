3fce78039e5caaa6a3c194b59c47d76428be1f51
Raz Besaleli <rbesaleli@gmail.com>
2025-03-02 21:48:07 -0500

    update config.toml

--- ptolemy/build.rs
+++ ptolemy/build.rs
@@ -90,7 +90,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Only happens if env var BUILD_PROTOBUFS is set to 1
     let build_protobufs = std::env::var("BUILD_PROTOBUFS")
         .map(|s| s == "1")
-        .unwrap_or(true);
+        .unwrap_or(false);
 
     if build_protobufs {
         tonic_build::configure()
