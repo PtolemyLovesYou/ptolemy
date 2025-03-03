54ef8bd0a3b0b458507f9f6851a6e8025d6357a2
Raz Besaleli <80226248+besaleli@users.noreply.github.com>
2025-03-02 22:46:04 -0500

    chore: Publish Python Client (#177)
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * chore: update python_release
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml
    
    * update config.toml

--- ptolemy/build.rs
+++ ptolemy/build.rs
@@ -90,10 +90,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Only happens if env var BUILD_PROTOBUFS is set to 1
     let build_protobufs = std::env::var("BUILD_PROTOBUFS")
         .map(|s| s == "1")
-        .unwrap_or(true);
+        .unwrap_or(false);
 
     if build_protobufs {
         tonic_build::configure()
+            .protoc_arg("--experimental_allow_proto3_optional")
             .build_server(true)
             .out_dir("src/generated")
             .compile_protos(
