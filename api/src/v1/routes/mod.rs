14ecf297f16d8aea0ce3c0beb309528bbffaf835
Raz Besaleli <besaleli@mac.mynetworksettings.com>
2025-07-15 08:39:07 -0400

    add test v1

--- api/src/v1/routes/mod.rs
+++ api/src/v1/routes/mod.rs
@@ -2,8 +2,18 @@ use super::{super::middleware::get_cors_layer, state::PtolemyState};
 use axum::Router;
 
 pub async fn get_router(state: PtolemyState) -> Router {
+    let publisher_service = ptolemy::generated::observer::record_publisher_server::RecordPublisherServer::new(
+        super::services::RecordPublisherService::new(state.clone())
+    );
+
+    let grpc_router = tonic::service::Routes::builder()
+        .routes()
+        .add_service(publisher_service)
+        .into_axum_router();
+
     Router::new()
         .route("/ping", axum::routing::get(|| async move { "Pong!" }))
         .with_state(state)
+        .merge(grpc_router)
         .layer(get_cors_layer())
 }
