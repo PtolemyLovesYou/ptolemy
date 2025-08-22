aa46ebf59a6abb1319f34594a6e6bdcd455d53df
Raz Besaleli <rbesaleli@gmail.com>
2025-08-22 10:13:34 +0400

    remove old enums

--- ptolemy/src/models/enums.rs
+++ ptolemy/src/models/enums.rs
@@ -20,36 +20,6 @@ serialize_enum!(
 );
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum ApiKeyPermission {
-    ReadOnly,
-    WriteOnly,
-    ReadWrite,
-}
-
-serialize_enum!(
-    ApiKeyPermission,
-    ShoutySnakeCase,
-    [ReadOnly, WriteOnly, ReadWrite]
-);
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum UserStatus {
-    Active,
-    Suspended,
-}
-
-serialize_enum!(UserStatus, ShoutySnakeCase, [Active, Suspended]);
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum WorkspaceRole {
-    User,
-    Manager,
-    Admin,
-}
-
-serialize_enum!(WorkspaceRole, ShoutySnakeCase, [User, Manager, Admin]);
-
-#[derive(Clone, Debug, PartialEq)]
 pub enum Tier {
     System,
     Subsystem,
--- ptolemy/src/models/mod.rs
+++ ptolemy/src/models/mod.rs
@@ -1,14 +1,7 @@
-mod auth;
 mod enums;
-mod event;
 mod id;
 mod json;
 
-pub use auth::{ServiceApiKey, User, UserApiKey, Workspace, WorkspaceUser};
-pub use enums::{ApiKeyPermission, FieldValueType, Tier, UserStatus, WorkspaceRole};
-pub use event::{
-    Proto, ProtoEvent, ProtoFeedback, ProtoInput, ProtoMetadata, ProtoOutput, ProtoRecord,
-    ProtoRuntime,
-};
+pub use enums::{FieldValueType, Tier};
 pub use id::Id;
 pub use json::JSON;
