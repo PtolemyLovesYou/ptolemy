6a5789d30d931fe3578c0eb2bb629f99bd8f505b
Raz Besaleli <80226248+besaleli@users.noreply.github.com>
2025-08-31 20:21:49 +0400

    feat(kafka): Kafka Connector (#320)
    * add docker services
    
    * update
    
    * kafka producer
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fmt
    
    * update
    
    * update
    
    * update
    
    * fmt
    
    * update
    
    * update

--- api/src/config/mod.rs
+++ api/src/config/mod.rs
@@ -1,13 +1,15 @@
 use figment::{
-    providers::{Format, Yaml},
+    providers::{Env, Format, Serialized, Yaml},
     Figment,
 };
 use serde::{Deserialize, Serialize};
 
 use crate::error::ApiError;
 
+use self::kafka::KafkaConfig;
 use self::stdout::StdoutConfig;
 
+pub mod kafka;
 pub mod stdout;
 
 pub mod serialization_method {
@@ -21,23 +23,32 @@ pub mod serialization_method {
     }
 }
 
-const DEFAULT_CONFIG: &'static str = "
-buffer_size: 1024
-sink_timeout_secs: 10
-";
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PtolemyConfig {
     pub buffer_size: usize,
     pub sink_timeout_secs: usize,
     pub stdout: Option<StdoutConfig>,
+    pub kafka: Option<KafkaConfig>,
+}
+
+impl Default for PtolemyConfig {
+    fn default() -> Self {
+        Self {
+            buffer_size: 1024,
+            sink_timeout_secs: 10,
+            stdout: None,
+            kafka: None,
+        }
+    }
 }
 
 impl PtolemyConfig {
     pub fn from_file() -> Result<Self, ApiError> {
-        Figment::new()
-            .merge(Yaml::string(DEFAULT_CONFIG))
-            .merge(Yaml::file("ptolemy.yml"))
+        let config_path = std::env::var("PTOLEMY_CONFIG").unwrap_or_else(|_| "ptolemy.yml".into());
+
+        Figment::from(Serialized::defaults(Self::default()))
+            .merge(Yaml::file(config_path))
+            .merge(Env::prefixed("PTOLEMY_"))
             .extract()
             .map_err(|e| {
                 tracing::error!("{:?}", e);
